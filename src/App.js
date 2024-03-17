468abb7e0763191a8c8484a5afa6a2b59c3b01fd
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-03-17 10:13:16 +0600

    Add application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import { useEffect, useState } from 'react';
+import WeatherWidget from './components/WeatherWidget';
+import {clear} from "@testing-library/user-event/dist/clear";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [backgroundColor, setBackgroundColor] = useState(
+        'linear-gradient(0deg, rgba(59,57,83,1) 0%, rgba(1,31,53,1) 100%)'
+    );
+    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit' }));
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentTime(new Date().toLocaleTimeString([], {hour: '2-digit'}));
+        });
+
+        if (currentTime >= 5 && currentTime < 13) {
+            setBackgroundColor(
+                'linear-gradient(0deg, rgba(242,186,166,1) 0%, rgba(151,199,226,1) 100%)'
+            );
+        } else if (currentTime >= 13 && currentTime < 18) {
+            setBackgroundColor(
+                'linear-gradient(0deg, rgba(143,187,215,1) 0%, rgba(72,132,173,1) 100%)'
+            );
+        } else if (currentTime >= 18 && currentTime < 22) {
+            setBackgroundColor(
+                'linear-gradient(0deg, rgba(191,101,93,1) 0%, rgba(111,111,140,1) 100%)'
+            );
+        } else if (currentTime < 5 || currentTime >= 22) {
+            setBackgroundColor(
+                'linear-gradient(0deg, rgba(59,57,83,1) 0%, rgba(1,31,53,1) 100%)'
+            );
+        }
+
+        return (() => { clearInterval(timer); })
+    }, []);
+
+    return (
+        <div className="App" style={{ background: backgroundColor }}>
+            <WeatherWidget />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/WeatherWidget.jsx
@@ -0,0 +1,94 @@
+import '../styles/WeatherWidget.scss';
+import React, { useEffect, useState } from 'react';
+
+const WeatherWidget = () => {
+    const dateInfo = new Date();
+    const dayNames = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ];
+    const dayOfWeek = dayNames[dateInfo.getDay()];
+    const dayOfMonth = dateInfo.getDate();
+    const monthNames = [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Остябрь',
+        'Ноябрь',
+        'Декабрь'
+    ];
+    const month = monthNames[dateInfo.getMonth()];
+    const currentTime = dateInfo.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+
+    const [weatherConditions, setWeatherConditions] = useState({
+        WeatherIcon: 1,
+        WeatherText: '',
+        Temperature: {
+            Metric: {
+                Value: '-0.0'
+            }
+        },
+        HasPrecipitation: false,
+        PrecipitationType: 0
+    });
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        fetch(
+            'http://dataservice.accuweather.com/currentconditions/v1/294463?apikey=%20%09KANPq6J8AWU1iaWP4iJEb32tQ7MVmpOX&language=ru-ru&details=false'
+        )
+            .then((response) => response.json())
+            .then((res) => setWeatherConditions(res[0]))
+            .catch((err) => setError(err.toString()));
+
+        setError('');
+    }, [weatherConditions]);
+
+    return (
+        <div className="weather-widget">
+            <div className="weather-widget__location-block">
+                Омск, {dayOfWeek}, {month} {dayOfMonth}, {currentTime}
+            </div>
+            <div className="weather-widget__weather-block">
+                <div className="weather-widget__img-text">
+                    <img
+                        className="weather-widget__img"
+                        src={`https://apidev.accuweather.com//developers/Media/Default/WeatherIcons/${weatherConditions.WeatherIcon < 10 ? '0' + weatherConditions.WeatherIcon : weatherConditions.WeatherIcon}-s.png`}
+                        alt={weatherConditions.WeatherText}
+                    />
+                    <div className="weather-widget__text">
+                        {weatherConditions.WeatherText}
+                    </div>
+                </div>
+                <div className="weather-widget__weather-info">
+                    <p className="weather-widget__temperature">
+                        {weatherConditions.Temperature.Metric.Value}°С
+                    </p>
+                    <p className="weather-widget__precipitation">
+                        Осадки:
+                        {!weatherConditions.HasPrecipitation
+                            ? ' Нет'
+                            : ' ' + weatherConditions.PrecipitationType}
+                    </p>
+                </div>
+            </div>
+            <p className="weather-widget__error">{error}</p>
+        </div>
+    );
+};
+
+export default WeatherWidget;
